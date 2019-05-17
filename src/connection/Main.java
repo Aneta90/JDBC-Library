d0a408f97c25adf2782783621b1d6bd9ba38c650
Aneta <aw22@onet.pl>
2019-05-17 17:56:56 +0200

    Comments removed.

--- src/connection/Main.java
+++ src/connection/Main.java
@@ -1,17 +1,4 @@
 package connection;
-import model.BookCopy;
-import model.Customer;
-import model.RentStatus;
-import service.OperationsOnBookCopy;
-import service.OperationsOnBookInfo;
-import service.OperationsOnCustomer;
-import service.OperationsOnRents;
-
-import java.math.BigDecimal;
-import java.sql.SQLException;
-import java.text.ParseException;
-import java.time.LocalDate;
-
 
 public class Main {
 
